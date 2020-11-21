28f056d00811fbf83a5670a51261f9e6902f541f
Sudha1404 <nsudharani.testing@gmail.com>
2020-11-21 15:55:05 +0530

    21 Nov commit

---
+++ MavenJava/src/test/java/Maven/MavenJava/SeleniumTest.java
@@ -0,0 +1,18 @@
+package Maven.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest 
+{
+	@Test
+	public void browserAutomation()
+	{
+		System.out.println("browserAutomation");
+	}
+	@Test
+	public void elementui()
+	{
+		System.out.println("elementui");
+	}
+
+}
