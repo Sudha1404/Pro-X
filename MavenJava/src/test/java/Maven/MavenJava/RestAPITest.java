cf40f645690d203897eb2bc2d89efbd6dbb777b3
Sudha1404 <nsudharani.testing@gmail.com>
2020-12-01 21:13:40 +0530

    1 Dec changes commit

--- MavenJava/src/test/java/Maven/MavenJava/RestAPITest.java
+++ MavenJava/src/test/java/Maven/MavenJava/RestAPITest.java
@@ -8,6 +8,11 @@ public class RestAPITest
 	public void postJira()
 	{
 		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+
 	}
 	@Test
 	public void deleteTwitter()
